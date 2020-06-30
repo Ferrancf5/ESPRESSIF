063fa6b9f18be239e452c9c74e4ca1e7f2fa8c89
Ferrancf5 <ferrancf96@gmail.com>
2020-06-30 23:15:10 +0200

    Ordenacion y subida de ficheros

---
+++ Arduino/Codigos_Prototipo/camion.ino
@@ -0,0 +1,209 @@
+#include <ArduinoJson.h>  // https://arduinojson.org/
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
+#include <WiFi.h>
+#include <Wire.h>
+#include "DHT.h" 
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+
+#define ECHO_PIN 12 // Analog input that receives the echo signal
+#define TRIG_PIN 13 // Digital output that sends the trigger signal
+#define DHT_PIN 22     
+#define DHT_TYPE DHT11 
+#define RX_PIN 16 // Pinout RX of ESP32
+#define TX_PIN 17 // Pinout TX of ESP32
+#define REFRESH_RATE 5000 // Defined in miliseconds
+
+
+
+
+// Replace the next variables with your Wi-Fi SSID/Password
+const char *WIFI_SSID = "Redmi";
+const char *WIFI_PASSWORD = "1638c7263f56";
+char macAddress[18];
+
+const char *MQTT_BROKER_IP = "10.20.60.5";
+const int MQTT_PORT = 1883;
+const char *MQTT_USER = "";
+const char *MQTT_PASSWORD = "";
+const bool RETAINED = true;
+
+float LAT;
+float LONG;
+
+HardwareSerial SerialGPS(1);
+DHT dhtSensor(22, DHT11);
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+TinyGPSPlus gps; 
+
+
+void setup() {
+  
+
+  Serial.begin(9600); // Starts the serial communication
+  Serial.println("\nBooting device...");
+
+  mqttClient.setServer(MQTT_BROKER_IP,
+                       MQTT_PORT); // Connect the configured mqtt broker
+  pinMode(ECHO_PIN, INPUT);  // Sets the ECHO_PIN as an Input
+  pinMode(TRIG_PIN, OUTPUT); // Sets the TRIG_PIN as an Output
+
+  dhtSensor.begin();
+
+  connectToWiFiNetwork(); // Connects to the configured network
+  connectToMqttBroker();  // Connects to the configured mqtt broker
+
+  SerialGPS.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN); // Starts gps communication with UART
+
+  TimerHandle_t xTimer = xTimerCreate("printGpsReadings", REFRESH_RATE, pdTRUE, (void *) 0, printGpsReadings);
+  xTimerStart(xTimer, 0);
+
+}
+
+void loop() {
+  checkConnections(); // We check the connection every time
+
+  // Publish every 2 seconds
+  static int nowTime = millis();
+  static int startTime = 0;
+  static int elapsedTime = 0;
+  nowTime = millis();
+  elapsedTime = nowTime - startTime;
+  if (elapsedTime >= 2000) {
+    
+    publishSmallJson();   // Publishes a small json
+    publishGPS();
+    startTime = nowTime;
+  }
+ if (SerialGPS.available()) {
+    gps.encode(SerialGPS.read()); // Encodes all messages from GPS
+  }
+}
+
+void printGpsReadings(TimerHandle_t xTimer){
+
+  LAT = gps.location.lat();
+  LONG = gps.location.lng();
+
+  Serial.print("LAT=");   Serial.println(String(LAT).c_str()); // Latitude in degrees (double)
+  Serial.print("LONG=");  Serial.println(String(LONG).c_str()); // Longitude in degrees (double)
+  Serial.print("SATS=");  Serial.println(gps.satellites.value()); // Number of satellites in use (u32)
+
+}
+
+void publishGPS() {
+
+  static const String topicGPS = createTopic("GPS");
+  static const char *topic = topicGPS.c_str();
+
+  StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
+
+  char buffer[128]; // Create the buffer where we will print the JSON document
+                    // to publish through MQTT
+  doc["lat"] = LAT;
+  doc["long"] = LONG;
+
+  // Serialize the JSON document to a buffer in order to publish it
+
+  serializeJson(doc, buffer);
+  mqttClient.publish(topic, buffer, RETAINED);
+  Serial.println(" <= " + String(topic) + ": " + String(buffer));
+
+}
+
+void publishSmallJson() {
+  static const String topicStr = createTopic("small_json");
+  static const char *topic = topicStr.c_str();
+  static float temperature;
+  temperature = dhtSensor.readTemperature();
+  
+   static float humidity; // Variable that will store the last humidity value
+  humidity = dhtSensor.readHumidity();  
+  
+  static float distance;
+  
+  distance = getDistance();
+   
+  StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
+  char buffer[128]; // Create the buffer where we will print the JSON document
+                    // to publish through MQTT
+  
+
+  doc["device"] = "ESP32"; // Add names and values to the JSON document
+  doc["sensor"] = "DHT22";
+  doc["sensor1"] = "Ultrasonido";
+  JsonObject values1 =
+      doc.createNestedObject("values1"); // We can add another Object
+  values1["t"] = temperature;
+  values1["h"] = humidity;  
+  values1["d"] = distance;
+
+
+  // Serialize the JSON document to a buffer in order to publish it
+  serializeJson(doc, buffer);
+  mqttClient.publish(topic, buffer, RETAINED);
+  Serial.println(" <= " + String(topic) + ": " + String(buffer));
+}
+
+float getDistance() {
+  digitalWrite(13, LOW); // Clear the TRIG_PIN by setting it LOW
+  delayMicroseconds(5);
+
+  // Trigger the sensor by setting the TRIG_PIN to HIGH for 10 microseconds
+  digitalWrite(13, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(13, LOW);
+
+  long duration = pulseIn(12, HIGH); // pulseIn() returns the duration (length of the pulse) in microseconds
+
+  return duration * 0.034 / 2; // Returns the distance in cm
+}
+
+String createTopic(char *topic) {
+  String topicStr = String(macAddress) + "/" + topic;
+  return topicStr;
+}
+
+void connectToWiFiNetwork() {
+  Serial.print(
+      "Connecting with Wi-Fi: " +
+      String(WIFI_SSID)); // Print the network which you want to connect
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".."); // Connecting effect
+  }
+  Serial.print("..connected!  (ip: "); // After being connected to a network,
+                                       // our ESP32 should have a IP
+  Serial.print(WiFi.localIP());
+  Serial.println(")");
+  String macAddressStr = WiFi.macAddress().c_str();
+  strcpy(macAddress, macAddressStr.c_str());
+}
+
+void connectToMqttBroker() {
+  Serial.print(
+      "Connecting with MQTT Broker:" +
+      String(MQTT_BROKER_IP));    // Print the broker which you want to connect
+  mqttClient.connect(macAddress, MQTT_USER, MQTT_PASSWORD);// Using unique mac address from ESP32
+  while (!mqttClient.connected()) {
+    delay(500);
+    Serial.print("..");             // Connecting effect
+    mqttClient.connect(macAddress); // Using unique mac address from ESP32
+  }
+  Serial.println("..connected! (ClientID: " + String(macAddress) + ")");
+}
+
+void checkConnections() {
+  if (mqttClient.connected()) {
+    mqttClient.loop();
+  } else { // Try to reconnect
+    Serial.println("Connection has been lost with MQTT Broker");
+    if (WiFi.status() != WL_CONNECTED) { // Check wifi connection
+      Serial.println("Connection has been lost with Wi-Fi");
+      connectToWiFiNetwork(); // Reconnect Wifi
+    }
+    connectToMqttBroker(); // Reconnect Server MQTT Broker
+  }
+}
---
+++ Arduino/Codigos_Prototipo/subscribeLED.ino
@@ -0,0 +1,136 @@
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
+#include <WiFi.h>
+#include <Wire.h>
+
+#define LED_PIN 25
+
+// Replace the next variables with your Wi-Fi SSID/Password
+const char *WIFI_SSID = "AulaAutomatica";
+const char *WIFI_PASSWORD = "ticsFcim";
+char macAddress[18];
+
+// Add MQTT Broker settings
+const char *MQTT_BROKER_IP = "10.20.60.5";
+const int MQTT_PORT = 1883;
+const char *MQTT_USER = "";
+const char *MQTT_PASSWORD = "";
+const bool RETAINED = true;
+const int QoS = 0; // Quality of Service for the subscriptions
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+void setup() {
+  Serial.begin(9600); // Starts the serial communication
+  Serial.println("\nBooting device...");
+
+  pinMode(LED_PIN, OUTPUT); // Pinout as output
+  
+  mqttClient.setServer(MQTT_BROKER_IP,MQTT_PORT); // Connect the configured mqtt broker
+  mqttClient.setCallback(
+      callback); // Prepare what to do when a message is recieved
+
+  connectToWiFiNetwork(); // Connects to the configured network
+  connectToMqttBroker();  // Connects to the configured mqtt broker
+  setSubscriptions();     // Subscribe defined topics
+}
+
+void loop() {
+  checkConnections(); // We check the connection every time
+}
+
+/* Additional functions */
+void setSubscriptions() {
+  //subscribe("GPS");
+  subscribe("LED");
+}
+
+void subscribe(char *newTopic) {
+  const String topicStr = createTopic(newTopic);
+  const char *topic = topicStr.c_str();
+  mqttClient.subscribe(topic, QoS);
+  Serial.println("Client MQTT subscribed to topic: " + topicStr +
+                 " (QoS:" + String(QoS) + ")");
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  // Register all subscription topics
+  //static const String gpsTopicStr = createTopic("GPS");
+  static const String ledTopic = createTopic("LED");
+
+  String msg = unwrapMessage(payload, length);
+  Serial.println(" => " + String(topic) + ": " + msg);
+
+  // What to do in each topic case?
+  if (String(topic) == ledTopic) {
+    if (msg == "1"){
+      digitalWrite(LED_PIN, HIGH); // Set pin to HIGH
+    }
+    else{
+      digitalWrite(LED_PIN, LOW); // Set pin to HIGH
+    }
+    
+  } else {
+    Serial.println("[WARN] - '" + String(topic) +
+                   "' topic was correctly subscribed but not defined in the "
+                   "callback function");
+  }
+}
+
+String unwrapMessage(byte *message, unsigned int length) {
+  String msg;
+  for (int i = 0; i < length; i++) { // Unwraps the string message
+    msg += (char)message[i];
+  }
+  return msg;
+}
+
+String createTopic(char *topic) {
+  String topicStr = String(macAddress) + "/" + topic;
+  return topicStr;
+}
+
+void connectToWiFiNetwork() {
+  Serial.print(
+      "Connecting with Wi-Fi: " +
+      String(WIFI_SSID)); // Print the network which you want to connect
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".."); // Connecting effect
+  }
+  Serial.print("..connected!  (ip: "); // After being connected to a network,
+                                       // our ESP32 should have a IP
+  Serial.print(WiFi.localIP());
+  Serial.println(")");
+  String macAddressStr = WiFi.macAddress().c_str();
+  //String macAddressStr = "24:6F:28:87:F0:88";
+  strcpy(macAddress, macAddressStr.c_str());
+}
+
+void connectToMqttBroker() {
+  Serial.print(
+      "Connecting with MQTT Broker: " +
+      String(MQTT_BROKER_IP));    // Print the broker which you want to connect
+  mqttClient.connect(macAddress); //(macAdress,MQTT_USER, MQTT_PASSWORD); // Using unique mac address from ESP32
+  while (!mqttClient.connected()) {
+    delay(500);
+    Serial.print("..");             // Connecting effect
+    mqttClient.connect(macAddress); // Using unique mac address from ESP32
+  }
+  Serial.println("..connected! (ClientID: " + String(macAddress) + ")");
+}
+
+void checkConnections() {
+  if (mqttClient.connected()) {
+    mqttClient.loop();
+  } else { // Try to reconnect
+    Serial.println("Connection has been lost with MQTT Broker");
+    if (WiFi.status() != WL_CONNECTED) { // Check wifi connection
+      Serial.println("Connection has been lost with Wi-Fi");
+      connectToWiFiNetwork(); // Reconnect Wifi
+    }
+    connectToMqttBroker(); // Reconnect Server MQTT Broker
+    setSubscriptions();    // Subscribes to configured topics
+  }
+}

e826ed9d2acacc7567158c20f644bd5a75809bec
FerranCIM <47112750x@cimupc.org>
2020-07-06 12:05:18 +0200

    Codigos Tanque i Camion actualizados

--- Arduino/Codigos Prototipo/tanque/tanque.ino
+++ Arduino/Codigos Prototipo/tanque/tanque.ino
@@ -2,50 +2,38 @@
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
 #include <WiFi.h>
 #include <Wire.h>
-#include "DHT.h" 
-#include <HardwareSerial.h>
-#include <TinyGPS++.h>
-
+#include "DHT.h"
+ 
 #define ECHO_PIN 12 // Analog input that receives the echo signal
 #define TRIG_PIN 13 // Digital output that sends the trigger signal
-#define DHT_PIN 22     
+#define DHT_PIN 21     
 #define DHT_TYPE DHT11 
-#define RX_PIN 16 // Pinout RX of ESP32
-#define TX_PIN 17 // Pinout TX of ESP32
-#define REFRESH_RATE 5000 // Defined in miliseconds
-
-
-
 
 // Replace the next variables with your Wi-Fi SSID/Password
-const char *WIFI_SSID = "Redmi";
-const char *WIFI_PASSWORD = "1638c7263f56";
+const char *WIFI_SSID = "ESPRESSIF";
+const char *WIFI_PASSWORD = "Espressif123";
 char macAddress[18];
 
-const char *MQTT_BROKER_IP = "10.20.60.5";
+const char *MQTT_BROKER_IP = "192.168.5.1";
 const int MQTT_PORT = 1883;
 const char *MQTT_USER = "";
 const char *MQTT_PASSWORD = "";
 const bool RETAINED = true;
 
-float LAT;
-float LONG;
-
-HardwareSerial SerialGPS(1);
-DHT dhtSensor(22, DHT11);
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
-TinyGPSPlus gps; 
+DHT dhtSensor(21, DHT11);
+
+static float temperature;
+static float humidity; 
+static float distance;
 
 
 void setup() {
-  
-
   Serial.begin(9600); // Starts the serial communication
   Serial.println("\nBooting device...");
 
-  mqttClient.setServer(MQTT_BROKER_IP,
-                       MQTT_PORT); // Connect the configured mqtt broker
+  mqttClient.setServer(MQTT_BROKER_IP, MQTT_PORT); // Connect the configured mqtt broker
   pinMode(ECHO_PIN, INPUT);  // Sets the ECHO_PIN as an Input
   pinMode(TRIG_PIN, OUTPUT); // Sets the TRIG_PIN as an Output
 
@@ -53,12 +41,6 @@ void setup() {
 
   connectToWiFiNetwork(); // Connects to the configured network
   connectToMqttBroker();  // Connects to the configured mqtt broker
-
-  SerialGPS.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN); // Starts gps communication with UART
-
-  TimerHandle_t xTimer = xTimerCreate("printGpsReadings", REFRESH_RATE, pdTRUE, (void *) 0, printGpsReadings);
-  xTimerStart(xTimer, 0);
-
 }
 
 void loop() {
@@ -71,74 +53,36 @@ void loop() {
   nowTime = millis();
   elapsedTime = nowTime - startTime;
   if (elapsedTime >= 2000) {
-    
+    readValues();
     publishSmallJson();   // Publishes a small json
-    publishGPS();
     startTime = nowTime;
   }
- if (SerialGPS.available()) {
-    gps.encode(SerialGPS.read()); // Encodes all messages from GPS
-  }
+ 
 }
 
-void printGpsReadings(TimerHandle_t xTimer){
-
-  LAT = gps.location.lat();
-  LONG = gps.location.lng();
-
-  Serial.print("LAT=");   Serial.println(String(LAT).c_str()); // Latitude in degrees (double)
-  Serial.print("LONG=");  Serial.println(String(LONG).c_str()); // Longitude in degrees (double)
-  Serial.print("SATS=");  Serial.println(gps.satellites.value()); // Number of satellites in use (u32)
-
+void readValues(){
+  temperature = dhtSensor.readTemperature();
+  humidity = dhtSensor.readHumidity();
+  distance = getDistance();  
+  
 }
 
-void publishGPS() {
-
-  static const String topicGPS = createTopic("GPS");
-  static const char *topic = topicGPS.c_str();
-
-  StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
-
-  char buffer[128]; // Create the buffer where we will print the JSON document
-                    // to publish through MQTT
-  doc["lat"] = LAT;
-  doc["long"] = LONG;
-
-  // Serialize the JSON document to a buffer in order to publish it
-
-  serializeJson(doc, buffer);
-  mqttClient.publish(topic, buffer, RETAINED);
-  Serial.println(" <= " + String(topic) + ": " + String(buffer));
-
-}
 
 void publishSmallJson() {
   static const String topicStr = createTopic("small_json");
   static const char *topic = topicStr.c_str();
-  static float temperature;
-  temperature = dhtSensor.readTemperature();
-  
-   static float humidity; // Variable that will store the last humidity value
-  humidity = dhtSensor.readHumidity();  
-  
-  static float distance;
-  
-  distance = getDistance();
    
   StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
   char buffer[128]; // Create the buffer where we will print the JSON document
                     // to publish through MQTT
   
-
   doc["device"] = "ESP32"; // Add names and values to the JSON document
   doc["sensor"] = "DHT22";
   doc["sensor1"] = "Ultrasonido";
-  JsonObject values1 =
-      doc.createNestedObject("values1"); // We can add another Object
+  JsonObject values1 = doc.createNestedObject("values1"); // We can add another Object
   values1["t"] = temperature;
   values1["h"] = humidity;  
   values1["d"] = distance;
-
 
   // Serialize the JSON document to a buffer in order to publish it
   serializeJson(doc, buffer);
